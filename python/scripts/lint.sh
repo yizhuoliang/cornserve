07a843f52a693b5efdc9f3d172301d3c8e6470f8
Jae-Won Chung <jwnchung@umich.edu>
2025-03-06 17:22:35 -0500

    [CI] Add Python format and lint check (#8)

--- python/scripts/lint.sh
+++ python/scripts/lint.sh
@@ -6,10 +6,9 @@ echo ${BASH_SOURCE[0]}
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 if [[ -z $GITHUB_ACTION ]]; then
-  ruff check --target-version py311 cornserve tests
+  ruff format --target-version py311 cornserve tests
 else
-  set -e
-  ruff check --target-version py311 --check cornserve tests
+  ruff format --target-version py311 --check cornserve tests
 fi
 
 ruff check cornserve
