82189e731e6d03e179612250fa1d4f158a1790d6
Jeff (Junze) Ma <93145857+majunze2001@users.noreply.github.com>
2025-03-28 11:11:21 -0400

    [OpenTelemetry] Move Jaeger to `cornserve-system` namespace (#20)

--- python/cornserve/constants.py
+++ python/cornserve/constants.py
@@ -6,7 +6,7 @@ K8S_SIDECAR_SERVICE_NAME = "sidecar"
 K8S_TASK_DISPATCHER_HTTP_URL = "task-dispatcher:8000"
 K8S_TASK_DISPATCHER_GRPC_URL = "task-dispatcher:50051"
 K8S_RESOURCE_MANAGER_GRPC_URL = "resource-manager:50051"
-K8S_OTEL_GRPC_URL = "http://jaeger-collector.cornserve:4317"
+K8S_OTEL_GRPC_URL = "http://jaeger-collector.cornserve-system:4317"
 
 CONTAINER_IMAGE_TASK_MANAGER = "cornserve/task-manager:latest"
 CONTAINER_IMAGE_ERIC = "cornserve/eric:latest"
